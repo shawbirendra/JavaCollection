17b5ecdf5bad60c1fa7df9add9033cab1cf9f956
Birendra Kumar Shaw <krs.birendra@gmail.com>
2021-07-13 01:00:22 +0530

    added test code

--- src/com/techiebirendra/list/ArrayListTest.java
+++ src/com/techiebirendra/list/ArrayListTest.java
@@ -1,7 +1,20 @@
 package com.techiebirendra.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListTest {
 	public static void main(String[] args) {
-		// TO-DO
+		List<Integer> integerList = new ArrayList<>();
+		// we can add as many null values as we want in arraylist
+		integerList.add(null);
+		integerList.add(null);
+		integerList.add(null);
+		integerList.add(null);
+
+		integerList.forEach(i -> {
+			System.out.println(i);
+		});
+
 	}
 }
